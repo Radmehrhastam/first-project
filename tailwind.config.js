d4d070935369164e5ef7b338a1839d9894a003e3
Radmehrhastam <mojiscarface@gmail.com>
2025-05-08 18:16:47 +0430

    add index.html

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// module.exports = {
+
+//   theme: {
+//     extend: {
+
+//       fontFamily: {
+//         inter: "'Inter', sans-serif",
+//         playfair: "'Playfair Display', serif",
+//       },
+
+//       colors: {
+//         primary: '#6D1600',
+//         secondary: '#282A3A',
+//         border: '#D9D9D9',
+//       },
+//     },
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
